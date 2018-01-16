6efe53f5cc9da6b362072a7cb0660812ff7d0828
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-01-16 16:22:57 -0600

    Finished extend

--- spec/part2.js
+++ spec/part2.js
@@ -191,7 +191,11 @@
       it('should override properties found on the destination', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = {a: 'c'};
+        var source = {a: 'b'};
+        var extended = _.extend(destination, source);
+        
+        expect(extended.a).to.equal('b')
       });
 
       it('should not override properties not found in the source', function() {
--- src/underbar.js
+++ src/underbar.js
@@ -232,11 +232,18 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 0; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        arguments[0][key] = arguments[i][key];
+      }
+    }
+    return arguments[0];
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    
   };
 
 
